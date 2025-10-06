feddd271ca11f5e9fa8b577a36c5444eb423a220
Jorge <jorgejapao00@gmail.com>
2025-10-06 09:17:19 -0300

    app.js added with app class

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import { Router } from './routes/generalRoute.js';
 
 export default class App{
     constructor(){
     this.server = express();
     this.server.use(express.json());
+
+    const myRouter = new Router();
+
+    this.server.use('/api', myRouter.router)
     }
 }
