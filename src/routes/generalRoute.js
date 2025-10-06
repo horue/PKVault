cb77c640622e2d0abef1609fbc12c83ccfb58b7e
Jorge <jorgejapao00@gmail.com>
2025-10-06 09:27:26 -0300

    /info route added

---
+++ src/routes/generalRoute.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { GeneralController } from '../controllers/generalController.js';
+
+
+
+
+export class Router{
+    constructor(){
+        this.router = express.Router()
+
+
+        this.router.get('/connection', GeneralController.connectionTest);
+        this.router.get('/info', GeneralController.information);
+    }
+}
