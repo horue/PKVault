e0fcdb648baf4414440bd005f274dfb29d13ede0
Jorge <jorgejapao00@gmail.com>
2025-10-07 15:27:57 -0300

    program updated to get commands from child process

--- src/utils/PKVault/Program.cs
+++ src/utils/PKVault/Program.cs
@@ -1,8 +1,36 @@
-﻿partial class Program
+﻿using PKHeX.Core;
+
+partial class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        string saveFilePath = @"C:\Users\jorge\Desktop\POKEMON HG.sav";
-        Converter.Convert(saveFilePath);
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: PKVault.exe <option> [filePath]");
+            return;
+        }
+
+        int option = int.Parse(args[0]);
+        string filePath = args.Length > 1 ? args[1] : null;
+
+        switch (option)
+        {
+            case 1: // Convert save
+                if (filePath != null)
+                    Converter.Convert(filePath);
+                break;
+            case 2: // Read Pokémon
+                if (filePath != null)
+                    PKReader.ReadPkmn(filePath);
+                break;
+            case 3:
+                string tempPath = Converter.Convert(filePath);
+                PKReader.CreateIndex(tempPath);
+                Console.Write("Index created at " + tempPath);
+                break;
+            default:
+                Console.WriteLine("Invalid option.");
+                break;
+        }
     }
 }
