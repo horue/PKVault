f741c2ff530835c1a3ab801c22c59865ac0c1f92
Jorge <jorgejapao00@gmail.com>
2025-10-07 06:18:07 -0300

    new routers added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import { Router } from './routes/generalRoute.js';
+import { PKRouter } from './routes/pkRoutes.js';
+import { SaveRouter } from './routes/saveRoute.js';
 
-export default class App{
-    constructor(){
-    this.server = express();
-    this.server.use(express.json());
+export default class App {
+    constructor() {
+        this.server = express();
+        this.server.use(express.json());
 
-    const myRouter = new Router();
+        const myRouter = new Router();
+        const pkRouter = new PKRouter();
+        const saveRouter = new SaveRouter();
 
-    this.server.use('/api', myRouter.router)
+        this.server.use('/api', myRouter.router)
+        this.server.use('/api', pkRouter.router)
+        this.server.use('/api', saveRouter.router)
     }
 }
