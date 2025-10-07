1237a426805867037773d475258dde4688581e7c
Jorge <jorgejapao00@gmail.com>
2025-10-07 06:16:03 -0300

    run method updated with mongo connection

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import App from "./app.js";
+import { Mongo } from "./config/db.js";
 
 
-class Server{
-    static run() {
+class Server {
+    static async run() {
+        Mongo.connect();
+
         const app = new App();
         const PORT = 151;
 
+
         console.log('PKVault developed by horue.')
         console.log('All Pokémon-related names, sprites, designs, and intellectual properties are the sole property of Nintendo, The Pokémon Company, Creatures Inc., and Game Freak.');
-        app.server.listen(PORT, ()=> console.log('Server is running on port ' + PORT + "."));
+        app.server.listen(PORT, () => console.log('Server is running on port ' + PORT + "."));
     }
 }
 
-Server.run()
+await Server.run()
