e471ca971b0874e4b4359613b778723dbb4188ac
Jorge <jorgejapao00@gmail.com>
2025-10-07 06:12:07 -0300

    general controller updated with status code

--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -1,12 +1,11 @@
-export class GeneralController{
-    static async connectionTest (req, res){
+export class GeneralController {
+    static async connectionTest(req, res) {
         const data = {
             message: "This is a connection test.",
-            code: 200
         }
-        res.json(data);
+        res.status(200).send(data);
     };
-    static async information (req, res){
+    static async information(req, res) {
         const data = {
             message: "PKVault is a fan-made, non-commercial software project created solely for educational and research purposes. It is not affiliated with, endorsed by, or supported by Nintendo, Game Freak, or The Pokémon Company. All names, sprites, designs, and trademarks related to Pokémon remain the intellectual property of their respective owners. This project does not include, distribute, or reproduce any copyrighted game assets."
         };
