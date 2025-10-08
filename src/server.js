539de31cbc1bea128d42e17f219f82f2d570bf80
Jorge <jorgejapao00@gmail.com>
2025-10-08 12:14:54 -0300

    terminal log updated with colors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import App from "./app.js";
 import { Mongo } from "./config/db.js";
+import chalk from "chalk";
 
 
 class Server {
@@ -9,11 +10,13 @@ class Server {
         const app = new App();
         const PORT = 151;
 
-
-        console.log('PKVault developed by horue.')
-        console.log('All Pokémon-related names, sprites, designs, and intellectual properties are the sole property of Nintendo, The Pokémon Company, Creatures Inc., and Game Freak.');
-        app.server.listen(PORT, () => console.log('Server is running on port ' + PORT + "."));
+        console.log("--- About ---")
+        console.log(chalk.blueBright('PKVault developed by horue.'))
+        console.log(chalk.redBright('All Pokémon-related names, sprites, designs, and intellectual properties are the sole property of Nintendo, The Pokémon Company, Creatures Inc., and Game Freak.'));
+        console.log("--- Info ---")
+        app.server.listen(PORT, () => console.log(chalk.green('Server is running on port ' + PORT + ".")));
     }
 }
+
 
 await Server.run()
