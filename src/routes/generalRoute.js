2546e81b96be95479a85826a882326d2f19f3f21
Jorge <jorgejapao00@gmail.com>
2025-10-09 11:34:53 -0300

    new route added

--- src/routes/generalRoute.js
+++ src/routes/generalRoute.js
@@ -4,12 +4,14 @@ import { GeneralController } from '../controllers/generalController.js';
 
 
 
-export class Router{
-    constructor(){
+export class Router {
+    constructor() {
         this.router = express.Router()
 
 
         this.router.get('/connection', GeneralController.connectionTest);
         this.router.get('/info', GeneralController.information);
+        this.router.get('/temp', GeneralController.tempFolder);
+
     }
 }
