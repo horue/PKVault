c469f63b137c886cfed917f0ecf5568631722d7c
Jorge <jorgejapao00@gmail.com>
2025-10-09 11:34:33 -0300

    general controller to get temp folder path added

--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -1,3 +1,6 @@
+import { tempPath } from "./saveController.js";
+
+
 export class GeneralController {
     static async connectionTest(req, res) {
         const data = {
@@ -8,6 +11,13 @@ export class GeneralController {
     static async information(req, res) {
         const data = {
             message: "PKVault is a fan-made, non-commercial software project created solely for educational and research purposes. It is not affiliated with, endorsed by, or supported by Nintendo, Game Freak, or The Pokémon Company. All names, sprites, designs, and trademarks related to Pokémon remain the intellectual property of their respective owners. This project does not include, distribute, or reproduce any copyrighted game assets."
+        };
+        res.json(data);
+
+    }
+    static async tempFolder(req, res) {
+        const data = {
+            message: "Temp folder path:" + tempPath
         };
         res.json(data);
 
